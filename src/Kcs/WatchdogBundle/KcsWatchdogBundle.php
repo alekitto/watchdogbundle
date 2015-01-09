51f1aab06a010e9ff67c7cf30e53f0ffd09a2409
Alessandro Chitolina <alekitto@gmail.com>
2015-01-09 14:10:23 +0100

    Error trace is now saved as binary field

--- src/Kcs/WatchdogBundle/KcsWatchdogBundle.php
+++ src/Kcs/WatchdogBundle/KcsWatchdogBundle.php
@@ -2,8 +2,14 @@
 
 namespace Kcs\WatchdogBundle;
 
+use Kcs\Doctrine\Types\Type;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KcsWatchdogBundle extends Bundle
 {
+    public function boot()
+    {
+        // Ensure binary array doctrine type is present
+        Type::registerTypes();
+    }
 }
