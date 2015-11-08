b62336c7e43c9136e44f38ddb301a38514f14ddb
Alessandro Chitolina <alekitto@gmail.com>
2015-11-08 18:24:59 +0100

    fixed couchdb persister service generation

--- src/Kcs/WatchdogBundle/DependencyInjection/CompilerPass/DoctrinePersisterServicesGeneratorPass.php
+++ src/Kcs/WatchdogBundle/DependencyInjection/CompilerPass/DoctrinePersisterServicesGeneratorPass.php
@@ -4,6 +4,7 @@ namespace Kcs\WatchdogBundle\DependencyInjection\CompilerPass;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 class DoctrinePersisterServicesGeneratorPass implements CompilerPassInterface
 {
@@ -24,6 +25,7 @@ class DoctrinePersisterServicesGeneratorPass implements CompilerPassInterface
         $container->setDefinition('kcs.watchdog.persister.doctrine.orm', $orm);
 
         $couchdb = clone $definition;
+        $couchdb->replaceArgument(0, new Reference('doctrine_couchdb'));
         $couchdb->replaceArgument(1, 'Kcs\WatchdogBundle\CouchDocument\Error');
         $orm->setAbstract(false);
         $container->setDefinition('kcs.watchdog.persister.doctrine.couchdb', $orm);
