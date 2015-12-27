ac9b46da245ea8bc28e41584fd1f7d24517a46d5
Alessandro Chitolina <alekitto@gmail.com>
2015-12-27 15:17:03 +0100

    add more tests

--- src/Kcs/WatchdogBundle/Tests/Debug/ExceptionHandlerTest.php
+++ src/Kcs/WatchdogBundle/Tests/Debug/ExceptionHandlerTest.php
@@ -3,6 +3,7 @@
 namespace Kcs\WatchdogBundle\Tests\Debug;
 
 use Kcs\WatchdogBundle\Debug\ExceptionHandler;
+use Kcs\WatchdogBundle\Entity\Error;
 use Kcs\WatchdogBundle\Storage\StorageInterface;
 use Prophecy\Argument;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
@@ -49,5 +50,66 @@ class ExceptionHandlerTest extends \PHPUnit_Framework_TestCase
         $handler = new ExceptionHandlerTester(array("InvalidArgumentException"));
         $handler->setStorage($storage->reveal())->setTokenStorage($token->reveal());
         $handler->logException(new \InvalidArgumentException('TESTING'));
+    }
+
+    public function testLogException()
+    {
+        $storage = $this->prophesize('Kcs\WatchdogBundle\Storage\StorageInterface');
+        $storage->getNewEntity()->willReturn(new Error());
+        $storage->persist(Argument::type('Kcs\WatchdogBundle\Entity\AbstractError'))
+            ->shouldBeCalledTimes(1);
+        $token = $this->prophesize('Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface');
+        $token->getToken()
+            ->willReturn(null);
+
+        $handler = new ExceptionHandlerTester(array());
+        $handler->setStorage($storage->reveal())->setTokenStorage($token->reveal());
+        $handler->logException(new \InvalidArgumentException('TESTING'));
+    }
+
+    public function testLogExceptionLogsUserCorrectly()
+    {
+        $that = $this;
+        $storage = $this->prophesize('Kcs\WatchdogBundle\Storage\StorageInterface');
+        $storage->getNewEntity()->willReturn(new Error());
+        $storage->persist(Argument::type('Kcs\WatchdogBundle\Entity\AbstractError'))
+            ->shouldBeCalledTimes(1)
+            ->will(function($arguments) use ($that) {
+                $user = $arguments[0]->getUser();
+                $that->assertEquals('user_test', $user['username']);
+            });
+
+        $token = $this->prophesize('Symfony\Component\Security\Core\Authentication\Token\TokenInterface');
+        $token->getUsername()->willReturn('user_test');
+        $token->getAttributes()->willReturn(array('attr' => 'test'));
+
+        $tokenStorage = $this->prophesize('Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface');
+        $tokenStorage->getToken()->willReturn($token->reveal());
+
+        $handler = new ExceptionHandlerTester(array());
+        $handler->setStorage($storage->reveal())->setTokenStorage($tokenStorage->reveal());
+        $handler->logException(new \InvalidArgumentException('TESTING'));
+    }
+
+    public function testLogExceptionShouldSetSeverityOnErrorException()
+    {
+        $that = $this;
+        $exc = new \ErrorException('TEST', 0, E_USER_ERROR);
+
+        $storage = $this->prophesize('Kcs\WatchdogBundle\Storage\StorageInterface');
+        $storage->getNewEntity()->willReturn(new Error());
+        $storage->persist(Argument::type('Kcs\WatchdogBundle\Entity\AbstractError'))
+            ->shouldBeCalledTimes(1)
+            ->will(function($arguments) use ($that) {
+                $that->assertEquals(E_USER_ERROR, $arguments[0]->getLevel());
+            });
+
+        $token = $this->prophesize('Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface');
+        $token->getToken()
+            ->willReturn(null);
+
+        $handler = new ExceptionHandlerTester(array());
+        $handler->setStorage($storage->reveal())->setTokenStorage($token->reveal());
+        $handler->logException($exc);
     }
 }
